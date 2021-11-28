db007d80011abf1e71a625e72a5c7558356cc6e3
MarinaBir <38644699+MarinkaKozlova@users.noreply.github.com>
2021-11-28 16:39:46 +0500

    Создан пакет для моей первой программы

---
+++ sandbox/src/main/java/ru/stqa/javaQA/sandbox/Hello.java
@@ -0,0 +1,9 @@
+package ru.stqa.javaQA.sandbox;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	System.out.println("ru.stqa.javaQA.sandbox.Hello, world!");
+ }
+
+}
--- sandbox/src/main/java/ru/stqa/sandbox/Hello.java
+++
@@ -1,9 +0,0 @@
-package ru.stqa.sandbox;
-
-public class Hello {
-
-	public static void main(String[] args) {
-	System.out.println("ru.stqb.proba.Hello, world!");
- }
-
-}
