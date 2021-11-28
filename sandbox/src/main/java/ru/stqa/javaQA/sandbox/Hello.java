9d837d08d37d589dfed341467760b6f9c8b85b4f
MarinaBir <38644699+MarinkaKozlova@users.noreply.github.com>
2021-11-28 16:59:19 +0500

    Update Hello.java

--- sandbox/src/main/java/ru/stqa/javaQA/sandbox/Hello.java
+++ sandbox/src/main/java/ru/stqa/javaQA/sandbox/Hello.java
@@ -3,7 +3,7 @@ package ru.stqa.javaQA.sandbox;
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("ru.stqa.javaQA.sandbox.Hello, world!");
+	System.out.println("Hello, world!");
  }
 
 }
