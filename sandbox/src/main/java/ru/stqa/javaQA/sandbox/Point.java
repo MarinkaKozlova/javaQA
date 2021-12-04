b9b7636e9fc6cd7f1dbf9b89b4bbc18bddcbaf66
MarinaBir <38644699+MarinkaKozlova@users.noreply.github.com>
2021-12-05 00:26:40 +0500

    Выполнение дз

--- sandbox/src/main/java/ru/stqa/javaQA/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/javaQA/sandbox/Point.java
@@ -1,16 +1,14 @@
 package ru.stqa.javaQA.sandbox;
 
 public class Point {
-
-    public static void main(String[] args) {
+public static void main (String [] args) {
 
 
         PointTT P = new PointTT(43, -10, -20, 21);
         System.out.println("Расстояние между двумя точками =" + distance(P));
     }
-
-    public static double distance(PointTT P) {
-        return Math.sqrt((P.p3 - P.p1) * (P.p3 - P.p1) + (P.p4 - P.p2) * (P.p4 - P.p2));
+    public static double distance (PointTT P){
+        return Math.sqrt((P.p3-P.p1)*(P.p3-P.p1)+(P.p4-P.p2)*(P.p4-P.p2));
     }
 }
 
---
+++ sandbox/src/main/java/ru/stqa/javaQA/sandbox/Rectangle.java
@@ -0,0 +1,17 @@
+
+    package ru.stqa.javaQA.sandbox;
+
+    public class Rectangle {
+
+        public double a;
+        public double b;
+        public Rectangle(double a, double b){
+            this.a = a;
+            this.b = b;
+        }
+        public  double are () {
+
+            return this.a * this.b;
+        }
+    }
+
